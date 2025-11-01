b43b9118514ba4b998996a12a6cf2e1f28cfed5b
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-11-02 00:05:35 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  const newSrc = link.href;
+
+  largeImg.src = newSrc;
+  largeImg.alt = link.title;
+});
